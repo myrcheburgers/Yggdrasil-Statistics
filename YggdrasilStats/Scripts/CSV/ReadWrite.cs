0641cda18b15b325fd2953a2fd995902a044f1c9
Taylor <Taylor@WALRUS>
2017-08-30 18:30:30 -0400

    Deleted Microsoft.VisualBasic.FileIO (changed parse method)

--- YggdrasilStats/Scripts/CSV/ReadWrite.cs
+++ YggdrasilStats/Scripts/CSV/ReadWrite.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.VisualBasic.FileIO; //(had to add Microsoft.VisualBasic reference to framework assembly)
 
 namespace YggdrasilStats.Scripts.CSV
 {
